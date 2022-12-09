13b63df4f4c2cf0b4bdb2f5440c6e823f97c5153
ks3364a <satwani.k@northeastern.edu>
2022-12-08 23:42:28 -0500

    Questions repo and viewmodel

---
+++ app/src/main/java/com/example/mobiledevfinalproject/QuestionsRepository.java
@@ -0,0 +1,26 @@
+package com.example.mobiledevfinalproject;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class QuestionsRepository {
+    private QuestionsDao mQuestionDao;
+    private LiveData<List<Questions>> mAllQuestions;
+
+
+    public QuestionsRepository(Application application){
+
+        QuestionsRoomDatabase db = QuestionsRoomDatabase.getInstance(application);
+        mQuestionDao = db.wordDao();
+        mAllQuestions = mQuestionDao.getAllQuestions();
+
+
+    }
+
+    public LiveData<List<Questions>> getmAllQuestions(){
+        return mAllQuestions;
+    }
+}
