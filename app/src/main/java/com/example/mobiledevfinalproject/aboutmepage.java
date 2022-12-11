344c3a85cbb4b35d53ef5a05ebd8011aa5512faf
Parker H <parkerhentz@gmail.com>
2022-12-11 16:06:32 -0500

    adding in our about me and activity

---
+++ app/src/main/java/com/example/mobiledevfinalproject/aboutmepage.java
@@ -0,0 +1,17 @@
+package com.example.mobiledevfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aboutmepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutmepage);
+        getSupportActionBar().setTitle("About Me");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
